ad2feb3bffb02367920673d56ad09f6019552d59
Ralph Landon <ralph7c2@gmail.com>
2022-03-02 14:29:18 -0600

    Answer messages

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const wss = new WebSocket.Server({ port: 10000 })
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
+    ws.send(`ho! ${message}`)
   })
   ws.send('ho!')
 })
