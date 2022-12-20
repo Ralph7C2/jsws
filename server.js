77a6c85c9fc6a7b0e388f7306800a5d3fc134b8f
Ralph Landon <ralph7c2@gmail.com>
2022-12-20 10:19:45 -0700

    Update ws package, use yarn

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 10000 })
 
 wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-    ws.send(`ho! ${message}`)
+  ws.on('message', (data, isBinary) => {
+    const message = isBinary ? data : data.toString();
+    console.log(`Received message => ${message}`);
+    ws.send(`ho! ${message}`);
   })
   ws.send('ho!')
 })
